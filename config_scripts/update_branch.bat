284f6eed916ce6cb1d8557406bfeb7eb90586982
UsernameIsNotAvailiable <ethangalka73@gmail.com>
2025-08-10 08:30:28 -0700

    create script to get current branch

--- config_scripts/update_branch.bat
+++ config_scripts/update_branch.bat
@@ -1,6 +1,7 @@
 @echo off
+echo "hi"
 setlocal enabledelayedexpansion
 FOR /F "tokens=*" %%i IN ('git rev-parse --abbrev-ref HEAD') DO SET branch=%%i
-echo "!branch!"
+echo "got !branch!"
 echo return { branchName = "!branch!" } > ..\src\shared\.special\branch.luau
 endlocal
